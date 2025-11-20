faa19b87c553744057da51fb20eca9059d1c51c1
gitshubhsss <shubhamranjane16@gmail.com>
2025-11-21 00:07:44 +0530

    linked list construction

--- LinkedList/LinkedList Construction/Main.java
+++ LinkedList/LinkedList Construction/Main.java
@@ -30,6 +30,9 @@ class LinkedList {
         if (head == null) {// size =0
             head = newNode;
             tail = newNode;
+        } else if (head == tail) {// size 1
+            tail.next = newNode;
+            tail = newNode;
         } else {
             tail.next = newNode;
             tail = newNode;
@@ -84,7 +87,7 @@ class LinkedList {
         }
         Node temp = head;
 
-        // Go till the last index
+        // Go till the last tail
         while (temp.next != tail) {
             temp = temp.next;
         }
@@ -95,6 +98,105 @@ class LinkedList {
         return;
     }
 
+    public void removeFirst() {
+        if (head == null) { /// sise 0
+            System.out.println("LinkedList is empty");
+            return;
+        } else if (head == tail) {// size 1 //means head.next==null
+            head = null;
+            tail = null;
+        } else {// size n
+            // Node temp = head;
+            // head = temp.next;
+            // temp = null;
+
+            head = head.next;
+        }
+        this.size--;
+    }
+
+    public Node getNodeAt(int idx) {
+        if (idx < 0 || idx >= this.size) {
+            System.out.println("galat index");
+            return null;
+        }
+        Node temp = head;
+        int i = 0;// starting from the first node
+        while (i < idx) {
+            temp = temp.next;
+            i++;
+        }
+        return temp;
+    }
+
+    public void addNodeAt(int idx, int value) {
+        if (idx < 0 || idx > this.size) {
+            System.out.println("galat index");
+            return;
+        }
+
+        if (idx == 0) {
+            addFirst(value);
+            return;
+        }
+        if (idx == size) {
+            addLast(value);
+            return;
+        }
+
+        Node newNode = new Node(value);// new node
+
+        Node prevNode = getNodeAt(idx - 1);// getting the previes node
+
+        Node nextOfprevNode = prevNode.next; // getting the next of the prev node
+
+        prevNode.next = newNode; // previos ke next mein new node
+
+        newNode.next = nextOfprevNode; // new node ke next me next of prev
+
+        size++;
+
+    }
+
+    public void removeNodeAt(int idx) {
+        if (idx < 0 || idx > this.size) {
+            System.out.println("galat index");
+            return;
+        }
+
+        Node currNode = getNodeAt(idx);// 9k
+
+        Node nextNode = currNode.next;// 8k
+
+        Node prevNode = getNodeAt(idx - 1);// 7k
+
+        prevNode.next = null;// disconnect
+
+        prevNode.next = nextNode;// 7k.next=8k
+
+        size--;
+
+    }
+
+    public void reverseLinkedList() {
+        if (head == null) {
+            System.out.println("cant reverse");
+            return;
+        }
+        int start = 0;// start
+        int end = size / 2;
+        while (start < end) {
+            Node firstNode = getNodeAt(start);
+            Node lastNode = getNodeAt(size - 1 - start);
+
+            // swap the data only
+            int temp = firstNode.data;
+            firstNode.data = lastNode.data;
+            lastNode.data = temp;
+            start++;
+        }
+    }
+
 }
 
 class Main {
@@ -102,10 +204,15 @@ class Main {
 
         LinkedList ll = new LinkedList();
 
-        ll.addFirst(10);
-        ll.addFirst(20);
+        ll.addLast(23);
+        ll.addLast(43);
+        ll.addLast(13);
 
-        ll.removeLast();
+        System.out.println("printing the oute");
+
+        ll.display();
+
+        ll.reverseLinkedList();
 
         ll.display();
 
