3c3589e1e5f92c48b64fd4afa724d2b96032799a
gitshubhsss <shubhamranjane16@gmail.com>
2026-05-22 23:29:31 +0530

    binary search modified apporch

--- Leetcode Streek/Main.java
+++ Leetcode Streek/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 class Main {
 
     class Solution3660 {
@@ -74,6 +76,38 @@ class Main {
 
             }
         }
+    }
+
+    class Solution33 {
+        public static int search(int[] nums, int target) {
+
+            int start = 0;
+            int end = nums.length - 1;
+
+            while (start <= end) {
+
+                int mid = (start + end) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[start] <= nums[mid]) {// check if the left array is sorted or not
+                    if ((nums[start] <= target) && (target <= nums[mid])) {
+                        end = mid - 1;
+                    } else {
+                        start = mid + 1;
+                    }
+                } else {
+                    if ((nums[mid] <= target) && (target <= nums[end])) {// check if right array is sorted or not
+                        start = mid + 1;
+                    } else {
+                        end = mid - 1;
+                    }
+                }
+
+            }
+
+            return -1;
+        }
+
     }
 
     public static void main(String[] args) {
